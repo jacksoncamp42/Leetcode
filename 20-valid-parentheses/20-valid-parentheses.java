ca3932dd8b3fd687b41f42043361e266ce70c640
Jackson Camp <37753577+jacksoncamp42@users.noreply.github.com>
2022-06-27 17:50:48 -0700

    Time: 3 ms (57.59%), Space: 42.4 MB (17.15%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,33 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack();
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')','(');
-        map.put(']','[');
-        map.put('}','{');
-        char popped;
+        Stack<Character> stack = new Stack<Character>();
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
         
-        for(char a : s.toCharArray()) {
-            // (, [, {
-            if(map.containsValue(a)) {
-                stack.push(a);
+        for (char c: s.toCharArray()) {
+            if (c == '{' || c == '(' || c == '[') {
+                stack.add(c);
             }
-            // ), ], }
-            else if(map.containsKey(a)) {
+            else {
                 if(stack.isEmpty()) {
                     return false;
                 }
-                //Need to check that last open paranthesis matches the closed one
-                else {
-                    if(stack.pop() != map.get(a)) {
-                        return false;
-                    }
+                if(map.get(c) != stack.pop()) {
+                    return false;
                 }
             }
-        }
-        if(!stack.isEmpty()) {
-            return false;
-        }
-        return true;
+        }     
+        return stack.isEmpty();
     }
 }
