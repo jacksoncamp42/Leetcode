00f47a17e7a786e6ffefb28f7f425d8508d6e80c
Jackson Camp <37753577+jacksoncamp42@users.noreply.github.com>
2022-06-30 13:31:45 -0700

    Time: 0 ms (100.00%), Space: 51.2 MB (37.84%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,24 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int i = 0;
-    while (i < nums.length) {
-      if (nums[i] < nums.length && nums[i] != nums[nums[i]])
-        swap(nums, i, nums[i]);
-      else
-        i++;
-    }
-
-    // find the first number missing from its index, that will be our required number
-    for (i = 0; i < nums.length; i++)
-      if (nums[i] != i)
-        return i;
-
-    return nums.length;
-  }
-
-  private static void swap(int[] arr, int i, int j) {
-    int temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
+          int sum = 0;
+        int total = nums.length * (nums.length+1)/2;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+        }
+        return total - sum;
     }
 }
