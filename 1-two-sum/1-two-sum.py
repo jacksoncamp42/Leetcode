2114970a1c93dee31243f0cac171d302da671679
Jackson Camp <37753577+jacksoncamp42@users.noreply.github.com>
2022-07-16 20:31:57 -0700

    Time: 80 ms (62.20%), Space: 14.2 MB (86.16%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -3,7 +3,7 @@ class Solution(object):
         dif = {}
         for i in range(len(nums)): # O(n) - iterating through list
             difference = target - nums[i]
-            if nums[i] in dif.keys(): # O(1) - dictionary access
+            if nums[i] in dif: # O(1) - dictionary access
                 index1 = dif[nums[i]]
                 return [i, index1]
             else:
