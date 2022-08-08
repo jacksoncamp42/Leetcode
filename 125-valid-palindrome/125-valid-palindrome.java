5bf3d5fc602609e8cc96d8913c4d81f87c6807cb
Jackson Camp <37753577+jacksoncamp42@users.noreply.github.com>
2022-08-07 18:25:50 -0700

    Time: 4 ms (89.85%), Space: 43.9 MB (55.29%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,31 +2,20 @@ class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        s = s.toLowerCase();
-
-        while(left < right) {     
-            while(!Character.isLetterOrDigit(s.charAt(left))) {
-                if(left >= right) {
-                    return true;
-                }
+        
+        while(left < right) {
+            while(!Character.isLetterOrDigit(s.charAt(left)) && left < right) {
                 left++;
             }
-            while(!Character.isLetterOrDigit(s.charAt(right))) {
-                if(left >= right) {
-                    return true;
-                }
+            while(!Character.isLetterOrDigit(s.charAt(right)) && left < right) {
                 right--;
-            }   
-              
-            char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
-            if(leftChar != rightChar) {
+            }
+            if(Character.toUpperCase(s.charAt(left)) != Character.toUpperCase(s.charAt(right)) && left < right) {
                 return false;
             }
             left++;
             right--;
         }
-        
         return true;
     }
 }
