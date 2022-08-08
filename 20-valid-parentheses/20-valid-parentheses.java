cf319fc60fa39d7b61f29d9cd5b1e2b689777bba
Jackson Camp <37753577+jacksoncamp42@users.noreply.github.com>
2022-08-07 18:54:12 -0700

    Time: 2 ms (90.79%), Space: 42.7 MB (6.37%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -3,23 +3,19 @@ class Solution {
         Stack<Character> stack = new Stack<Character>();
         Map<Character, Character> map = new HashMap<Character, Character>();
         map.put(')', '(');
-        map.put('}', '{');
         map.put(']', '[');
-
+        map.put('}', '{');
         
-        for (char c: s.toCharArray()) {
-            if (c == '{' || c == '(' || c == '[') {
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '['  || c == '{') {
                 stack.add(c);
             }
             else {
-                if(stack.isEmpty()) {
-                    return false;
-                }
-                if(map.get(c) != stack.pop()) {
+                if(stack.isEmpty() || map.get(c) != stack.pop()) {
                     return false;
                 }
             }
-        }     
+        }
         return stack.isEmpty();
     }
 }
